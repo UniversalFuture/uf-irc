04a93a004ed0f802da01089001a8c563f562a845
regiostech <ultralammed@yahoo.com>
2015-10-14 05:40:08 -0400

    Updates.

--- js/controllers/ChatController.js
+++ js/controllers/ChatController.js
@@ -1,6 +1,10 @@
 angular.module('uf-irc').controller('ChatController', ['$scope', 'ircService', function ($scope, ircService) {
 	$scope.messages = [];
+	$scope.ircService = ircService;
+	for (var i = 0; i < ircService.channels.length; i++) {
+		$scope.messages[ircService.channels[i]] = [];
+	}
 	ircService.client.addListener('message', function (from, to, message) {
-		alert(from + "(to " + to + "): \"" + message + "\"");
+		$scope.messages[to].push({ "from": from, "message": message });
 	});
 }]);
